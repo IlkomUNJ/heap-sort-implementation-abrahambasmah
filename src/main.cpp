7a98665757a8249e0858c87677f872c55a816e72
abrahambasmah <abrahambasmah@gmail.com>
2025-03-10 21:10:54 +0700

    10 maret 2025

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void maxHeapify( int i , int *a , int n ){
+
+    int largest = i;
+    int left    = ( i * 2 ) + 1;
+    int right    = ( i * 2 ) + 2;
+
+    if( left < n && a[ largest] < a[ left ])
+        largest = left;
+    if( right < n  && a[ largest ] < a[right])
+        largest = right;
+    if( largest != i ){
+        swap( a[i], a[largest]);
+        maxHeapify( largest , a,  n );
+    }
+}
+
+
+int main(){
+    int n;
+    int * a;
+    cout << "masukan banyak angka : ";
+    cin >> n ;
+    cout << "masukan angka yang ingin di sort : ";
+    a = new int[n];
+    for( int i = 0; i < n ; i++){
+        cin >> a[i];
+    }
+
+    for( int i = n/2 -1 ; i >= 0 ; i-- ){
+        maxHeapify( i , a, n);
+    }
+    for(int i = 0; i < n ; i++){
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
